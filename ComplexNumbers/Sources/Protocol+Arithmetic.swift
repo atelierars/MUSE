fca535b79b95fbaa477e9ce419c5a6ab80b59622
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-08-16 15:50:00 +0900

    base arithmetics

---
+++ ComplexNumbers/Sources/Protocol+Arithmetic.swift
@@ -0,0 +1,82 @@
+//
+//  Protocol+Arithmetic.swift
+//  
+//
+//  Created by kotan.kn on 8/16/R6.
+//
+extension ComplexNumberProtocol {
+	@inline(__always)
+	@inlinable
+	public static func+(lhs: Self, rhs: Self) -> Self {
+		let real = lhs.real + rhs.real
+		let imag = lhs.imag + rhs.imag
+		return.init(real: real, imag: imag)
+	}
+	@inline(__always)
+	@inlinable
+	public static func-(lhs: Self, rhs: Self) -> Self {
+		let real = lhs.real - rhs.real
+		let imag = lhs.imag - rhs.imag
+		return.init(real: real, imag: imag)
+	}
+	@inline(__always)
+	@inlinable
+	public static func*(lhs: Self, rhs: Self) -> Self {
+		let real = lhs.real * rhs.real - lhs.imag * rhs.imag
+		let imag = lhs.imag * rhs.real + lhs.real * rhs.imag
+		return.init(real: real, imag: imag)
+	}
+}
+extension ComplexNumberProtocol {
+	@inline(__always)
+	@inlinable
+	public static func+=(lhs: inout Self, rhs: Self) {
+		lhs = lhs + rhs
+	}
+	@inline(__always)
+	@inlinable
+	public static func-=(lhs: inout Self, rhs: Self) {
+		lhs = lhs - rhs
+	}
+	@inline(__always)
+	@inlinable
+	public static func*=(lhs: inout Self, rhs: Self) {
+		lhs = lhs * rhs
+	}
+}
+extension ComplexNumberProtocol where FloatLiteralType: BinaryInteger {
+	@inline(__always)
+	@inlinable
+	public static func/(lhs: Self, rhs: Self) -> Self {
+		let norm = rhs.real * rhs.real + rhs.imag * rhs.imag
+		let real = lhs.real * rhs.real + lhs.imag * rhs.imag
+		let imag = lhs.real * rhs.imag - lhs.imag * rhs.real
+		return.init(real: real / norm, imag: imag / norm)
+	}
+	@inline(__always)
+	@inlinable
+	public static func/=(lhs: inout Self, rhs: Self) {
+		lhs = lhs / rhs
+	}
+}
+extension ComplexNumberProtocol where FloatLiteralType: BinaryFloatingPoint {
+	@inline(__always)
+	@inlinable
+	public static func/(lhs: Self, rhs: Self) -> Self {
+		let norm = rhs.real * rhs.real + rhs.imag * rhs.imag
+		let real = lhs.real * rhs.real + lhs.imag * rhs.imag
+		let imag = lhs.real * rhs.imag - lhs.imag * rhs.real
+		return.init(real: real / norm, imag: imag / norm)
+	}
+	@inline(__always)
+	@inlinable
+	public static func/=(lhs: inout Self, rhs: Self) {
+		lhs = lhs / rhs
+	}
+}
+
+extension ComplexNumberProtocol {
+	public static prefix func-(_ χ: Self) -> Self {
+		.init(real: -χ.real, imag: -χ.imag)
+	}
+}
