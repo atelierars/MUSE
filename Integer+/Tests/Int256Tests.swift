d7a9b087d4d73687ae37118417b840576aabe797
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-10-15 23:37:23 +0900

    signed class tests

---
+++ Integer+/Tests/Int256Tests.swift
@@ -0,0 +1,79 @@
+//
+//  Int256Tests.swift
+//	Integer+
+//
+//  Created by kotan.kn on 9/3/R6.
+//
+import Testing
+@testable import Integer_
+#if os(macOS)
+private typealias Seed = Int
+private typealias Type = Int256
+@Suite
+struct Int256Tests {
+	@Test
+	func add() {
+		let a = Seed.random(in: -1024...1024)
+		let b = Seed.random(in: -1024...1024)
+		let x = Type(integerLiteral: a)
+		let y = Type(integerLiteral: b)
+		#expect(x + y == Type(integerLiteral: a + b))
+	}
+	@Test
+	func sub() {
+		let a = Seed.random(in: 1024...2048)
+		let b = Seed.random(in: 0...1024)
+		let x = Type(integerLiteral: a)
+		let y = Type(integerLiteral: b)
+		#expect(x - y == Type(integerLiteral: a - b))
+	}
+	@Test
+	func mul() {
+		let a = Seed.random(in: -1024...1024)
+		let b = Seed.random(in: -1024...1024)
+		let x = Type(integerLiteral: a)
+		let y = Type(integerLiteral: b)
+		#expect(x * y == Type(integerLiteral: a * b))
+	}
+	@Test
+	func div() {
+		let a = Seed.random(in: -1024...1024)
+		let b = Seed.random(in: 1...1024)
+		let x = Type(integerLiteral: a)
+		let y = Type(integerLiteral: b)
+		#expect(x / y == Type(integerLiteral: a / b))
+	}
+	@Test
+	func EQ() {
+		let x = 1024 as Type
+		let y = 1024 as Type
+		#expect(x == y)
+	}
+	@Test
+	func GT() {
+		let x = 1025 as Type
+		let y = 1024 as Type
+		#expect(x > y)
+	}
+	@Test
+	func LT() {
+		let x = 1023 as Type
+		let y = 1024 as Type
+		#expect(x < y)
+	}
+	@Test
+	func trailing() {
+		let x = 65536 as Type
+		let y = x * x * x * x
+		#expect(y.trailingZeroBitCount == 65536.trailingZeroBitCount * 4)
+	}
+	@Test
+	func sigNum() {
+		let x = -63 as Type
+		#expect((x*0).signum() == 0)
+		#expect((x).signum() == -1)
+		#expect((x*x).signum() == 1)
+	}
+	
+}
+#endif
