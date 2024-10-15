ccdccb64bb90320a038dab8eb7d6cba09a331cb8
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-10-15 23:40:48 +0900

    employ Testing instead of XCTest

--- Real+/Tests/Real+Tests.swift
+++ Real+/Tests/Real+Tests.swift
@@ -1,15 +1,14 @@
 //
-//  File.swift
-//  
+//  Real+Tests.swift
+//  Real+
 //
 //  Created by kotan.kn on 8/16/R6.
 //
-import XCTest
-@testable import Real_
-final class BinaryFloatingPointTests: XCTestCase {
-	func testContinuedFractionExpansion() {
-		let x = 2.0.squareRoot()
-		let s = x.continuedFractionSequence(as: Int.self)
-		XCTAssertEqual(Array(s.prefix(12)), [1] + Array(repeating: 2, count: 11))
-	}
+import Testing
+import Real_
+@Test
+func continuedFractionExpansion() {
+	let x = 2.0.squareRoot()
+	let s = x.continuedFractionSequence(as: Int.self)
+	#expect(Array(s.prefix(12)) == [1] + Array(repeating: 2, count: 11))
 }
