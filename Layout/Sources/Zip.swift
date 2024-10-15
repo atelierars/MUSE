05464a53f198e9df1b6a4b08f6307ab9367a25f7
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-10-16 01:55:08 +0900

    more zip

---
+++ Layout/Sources/Zip.swift
@@ -0,0 +1,63 @@
+//
+//  Zip.swift
+//
+//
+//  Created by Kota on 8/21/R6.
+//
+@_disfavoredOverload
+@inlinable @inline(__always)
+public func zip<X, Y, Z>(_ x: some Sequence<X>, _ y: some Sequence<Y>, _ z: some Sequence<Z>) -> some Sequence<(X, Y, Z)> {
+	sequence(state: (x.makeIterator(), y.makeIterator(), z.makeIterator())) {
+		switch ($0.0.next(), $0.1.next(), $0.2.next()) {
+		case(.some(let x), .some(let y), .some(let z)):
+			.some((x, y, z))
+		default:
+			.none
+		}
+	}
+}
+@_disfavoredOverload
+@inlinable @inline(__always)
+public func zip<X, Y, Z, W>(_ x: some Sequence<X>, _ y: some Sequence<Y>, _ z: some Sequence<Z>, _ w: some Sequence<W>) -> some Sequence<(X, Y, Z, W)> {
+	sequence(state: (x.makeIterator(), y.makeIterator(), z.makeIterator(), w.makeIterator())) {
+		switch ($0.0.next(), $0.1.next(), $0.2.next(), $0.3.next()) {
+		case(.some(let x), .some(let y), .some(let z), .some(let w)):
+			.some((x, y, z, w))
+		default:
+			.none
+		}
+	}
+}
+@inlinable @inline(__always)
+public func zip<S: SIMDScalar>(_ x: some Sequence<S>, _ y: some Sequence<S>) -> some Sequence<SIMD2<S>> {
+	sequence(state: (x.makeIterator(), y.makeIterator())) {
+		switch ($0.0.next(), $0.1.next()) {
+		case(.some(let x), .some(let y)):
+			.some(.init(x, y))
+		default:
+			.none
+		}
+	}
+}
+@inlinable @inline(__always)
+public func zip<S: SIMDScalar>(_ x: some Sequence<S>, _ y: some Sequence<S>, _ z: some Sequence<S>) -> some Sequence<SIMD3<S>> {
+	sequence(state: (x.makeIterator(), y.makeIterator(), z.makeIterator())) {
+		switch ($0.0.next(), $0.1.next(), $0.2.next()) {
+		case(.some(let x), .some(let y), .some(let z)):
+			.some(.init(x, y, z))
+		default:
+			.none
+		}
+	}
+}
+@inlinable @inline(__always)
+public func zip<S: SIMDScalar>(_ x: some Sequence<S>, _ y: some Sequence<S>, _ z: some Sequence<S>, _ w: some Sequence<S>) -> some Sequence<SIMD4<S>> {
+	sequence(state: (x.makeIterator(), y.makeIterator(), z.makeIterator(), w.makeIterator())) {
+		switch ($0.0.next(), $0.1.next(), $0.2.next(), $0.3.next()) {
+		case(.some(let x), .some(let y), .some(let z), .some(let w)):
+			.some(.init(x, y, z, w))
+		default:
+			.none
+		}
+	}
+}
