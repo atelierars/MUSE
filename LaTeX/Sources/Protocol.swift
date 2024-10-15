1eb00101d2558ec910cb2b1130803113d0b82e44
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-10-15 23:33:06 +0900

    latex compatible to render

--- LaTeX/Sources/Protocol.swift
+++ LaTeX/Sources/Protocol.swift
@@ -1,9 +1,9 @@
 //
 //  Protocol.swift
-//
+//	LaTeX
 //
 //  Created by kotan.kn on 8/16/R6.
 //
-public protocol CustomLaTeXStringConvertible: CustomStringConvertible {
+public protocol CustomLaTeXStringConvertible {
 	var latexDescription: String { get }
 }
