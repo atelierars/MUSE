16363decd9e96d1c20edfded997eea641318e590
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-10-16 09:39:41 +0900

    fix

--- ComplexNumbers/Tests/ComplexTests.swift
+++ ComplexNumbers/Tests/ComplexTests.swift
@@ -1,11 +1,46 @@
 //
 //  ComplexTests.swift
-//  
+//  ComplexNumbers
 //
 //  Created by kotan.kn on 8/16/R6.
 //
-import XCTest
+import Testing
+import simd
 @testable import ComplexNumbers
-final class ComplexTests: XCTestCase {
-	
+@Test(arguments: [
+	(Complex64(real: 3, imag: 4), Complex64(real: 4, imag: 3))
+])
+func add(lhs: Complex64, rhs: Complex64) {
+	let result = lhs + rhs
+	#expect(lhs.real + rhs.real == result.real)
+	#expect(lhs.imag + rhs.imag == result.imag)
+}
+@Test(arguments: [
+	(Complex64(real: 3, imag: 4), Complex64(real: 4, imag: 3))
+])
+func sub(lhs: Complex64, rhs: Complex64) {
+	let result = lhs - rhs
+	#expect(lhs.real - rhs.real == result.real)
+	#expect(lhs.imag - rhs.imag == result.imag)
+}
+@Test(arguments: [
+	(Complex64(real: 3, imag: 4), Complex64(real: 4, imag: 3))
+])
+func mul(lhs: Complex64, rhs: Complex64) {
+	let result = lhs * rhs
+	let real = dot(unsafeBitCast(lhs, to: SIMD2<Complex64.FloatLiteralType>.self), unsafeBitCast(rhs.conjugate, to: SIMD2<Complex64.FloatLiteralType>.self))
+	let imag = dot(SIMD2(lhs.real, rhs.real), SIMD2(rhs.imag, lhs.imag))
+	#expect(result.real == real)
+	#expect(result.imag == imag)
+}
+@Test(arguments: [
+	(Complex64(real: 3, imag: 4), Complex64(real: 4, imag: 3))
+])
+func div(lhs: Complex64, rhs: Complex64) {
+	let result = lhs / rhs
+	let factor = length_squared(unsafeBitCast(rhs, to: SIMD2<Complex64.FloatLiteralType>.self))
+	let real = dot(unsafeBitCast(lhs, to: SIMD2<Complex64.FloatLiteralType>.self), unsafeBitCast(rhs, to: SIMD2<Complex64.FloatLiteralType>.self)) / factor
+	let imag = dot(SIMD2(lhs.real, rhs.real), SIMD2(-rhs.imag, lhs.imag)) / factor
+	#expect(result.real == real)
+	#expect(result.imag == imag)
 }
