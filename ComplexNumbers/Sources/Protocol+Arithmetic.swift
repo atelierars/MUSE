a9294b2e0cad41b3b2c52ec668e5a2f8d52cfd26
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-10-16 09:39:22 +0900

    fix

--- ComplexNumbers/Sources/Protocol+Arithmetic.swift
+++ ComplexNumbers/Sources/Protocol+Arithmetic.swift
@@ -5,22 +5,22 @@
 //  Created by kotan.kn on 8/16/R6.
 //
 extension ComplexNumber {
-	@inline(__always)
 	@inlinable
+	@inline(__always)
 	public static func+(lhs: Self, rhs: Self) -> Self {
 		let real = lhs.real + rhs.real
 		let imag = lhs.imag + rhs.imag
 		return.init(real: real, imag: imag)
 	}
-	@inline(__always)
 	@inlinable
+	@inline(__always)
 	public static func-(lhs: Self, rhs: Self) -> Self {
 		let real = lhs.real - rhs.real
 		let imag = lhs.imag - rhs.imag
 		return.init(real: real, imag: imag)
 	}
-	@inline(__always)
 	@inlinable
+	@inline(__always)
 	public static func*(lhs: Self, rhs: Self) -> Self {
 		let real = lhs.real * rhs.real - lhs.imag * rhs.imag
 		let imag = lhs.imag * rhs.real + lhs.real * rhs.imag
@@ -28,62 +28,62 @@ extension ComplexNumber {
 	}
 }
 extension ComplexNumber {
-	@inline(__always)
 	@inlinable
+	@inline(__always)
 	public static func+=(lhs: inout Self, rhs: Self) {
 		lhs = lhs + rhs
 	}
-	@inline(__always)
 	@inlinable
+	@inline(__always)
 	public static func-=(lhs: inout Self, rhs: Self) {
 		lhs = lhs - rhs
 	}
-	@inline(__always)
 	@inlinable
+	@inline(__always)
 	public static func*=(lhs: inout Self, rhs: Self) {
 		lhs = lhs * rhs
 	}
 }
 extension ComplexNumber where FloatLiteralType: BinaryInteger {
-	@inline(__always)
 	@inlinable
+	@inline(__always)
 	public static func/(lhs: Self, rhs: Self) -> Self {
 		let norm = rhs.real * rhs.real + rhs.imag * rhs.imag
 		let real = lhs.real * rhs.real + lhs.imag * rhs.imag
 		let imag = lhs.real * rhs.imag - lhs.imag * rhs.real
 		return.init(real: real / norm, imag: imag / norm)
 	}
-	@inline(__always)
 	@inlinable
+	@inline(__always)
 	public static func/=(lhs: inout Self, rhs: Self) {
 		lhs = lhs / rhs
 	}
 }
 extension ComplexNumber where FloatLiteralType: BinaryFloatingPoint {
-	@inline(__always)
 	@inlinable
+	@inline(__always)
 	public static func/(lhs: Self, rhs: Self) -> Self {
 		let norm = rhs.real * rhs.real + rhs.imag * rhs.imag
 		let real = lhs.real * rhs.real + lhs.imag * rhs.imag
 		let imag = lhs.real * rhs.imag - lhs.imag * rhs.real
 		return.init(real: real / norm, imag: imag / norm)
 	}
-	@inline(__always)
 	@inlinable
+	@inline(__always)
 	public static func/=(lhs: inout Self, rhs: Self) {
 		lhs = lhs / rhs
 	}
 }
 extension ComplexNumber {
-	@inline(__always)
 	@inlinable
+	@inline(__always)
 	public static prefix func-(_ χ: Self) -> Self {
 		.init(real: -χ.real, imag: -χ.imag)
 	}
 }
 extension ComplexNumber {
-	@inline(__always)
 	@inlinable
+	@inline(__always)
 	public static func==(lhs: Self, rhs: Self) -> Bool {
 		(lhs.real, lhs.imag) == (rhs.real, rhs.imag)
 	}
